7e3b1973efdc3a70dc1f5bcb81a880383c237e15
Tom Dawes <>
2021-02-08 23:30:35 +0000

    Add html and example images

--- js-example/main.js
+++ js-example/main.js
@@ -1,8 +1,42 @@
+class App
+{
+    fileInputController;
+    fileUploader;
+    log;
+
+    //entry point
+    constructor()
+    {
+        this.fileInputController = new FileInputController();
+        this.fileUploader = new FileUploader();
+        this.log = new Log();
+    }
+}
+
+class SkynetConfig 
+{
+    static get portal()
+    {
+        return 'siasky.net';
+    }
+}
+
 class FileUploader
 {
+    static instance;
+
+    constructor()
+    {
+        FileUploader.instance = this;
+    }
+
     async uploadFileToSkynet(file) 
     {
         return new Promise((resolve, reject) => {
+            var requestParams = new FormData();
+            requestParams.append("filename", "myFile");
+            requestParams.append("file", file);
+
             var url = `https://${SkynetConfig.portal}/skynet/skyfile`;
         
             var request = new XMLHttpRequest();
@@ -19,8 +53,57 @@ class FileUploader
                 reject("error");
             }
 
-            request.send(file);
+            console.log(new URLSearchParams(requestParams).toString());
+            request.send(requestParams);
         });
     }
 }
 
+class FileInputController
+{
+    static instance;
+
+    fileInput;
+    file;
+
+    constructor()
+    {
+        FileInputController.instance = this;
+        this.addButtonListener();
+    }
+
+    addButtonListener()
+    {
+        this.fileInput = document.getElementById("fileInput");
+        this.fileInput.onchange = () => this.uploadFile();
+    }
+
+    async uploadFile()
+    {
+        Log.instance.log("Uploading file...");
+
+        this.file = this.fileInput.files[0];
+        Log.instance.log(this.file);
+
+        let response = await FileUploader.instance.uploadFileToSkynet(this.file);
+        Log.instance.log(response);
+    }
+}
+
+class Log
+{
+    static instance;
+
+    constructor()
+    {
+        Log.instance = this;
+    }
+
+    log(message)
+    {
+        let logArea = document.getElementById("log");
+        logArea.innerText += message + "\n";
+    }
+}
+
+let app = new App();
