449fa9478a305c18cdb22bf6ec87f392ac38d624
Tom Dawes <>
2021-02-25 18:46:42 +0000

    Add DropViewModel

---
+++ SkyDrop/src/SkyDrop.Core/ViewModels/DropViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using SkyDrop.Core.DataModels;
+using SkyDrop.Core.DataViewModels;
+using SkyDrop.Core.Services;
+
+namespace SkyDrop.Core.ViewModels.Main
+{
+    public class DropViewModel : BaseViewModel
+    {
+        private readonly IApiService apiService;
+        private readonly IStorageService storageService;
+        private readonly IUserDialogs userDialogs;
+        private readonly IMvxNavigationService navigationService;
+
+        public IMvxCommand SendCommand { get; set; }
+        public IMvxCommand ReceiveCommand { get; set; }
+
+        public DropViewModel(ISingletonService singletonService,
+                             IApiService apiService,
+                             IStorageService storageService,
+                             IUserDialogs userDialogs,
+                             IMvxNavigationService navigationService,
+                             ILog log) : base(singletonService)
+        {
+            Title = "Drop";
+
+            this.apiService = apiService;
+            this.storageService = storageService;
+            this.userDialogs = userDialogs;
+            this.navigationService = navigationService;
+        }
+
+
+    }
+}
