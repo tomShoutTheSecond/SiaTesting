c3b52ad529fa21db960a9ca6a30be16e3217306f
Tom Dawes <>
2021-03-06 22:41:33 +0000

    Add upload button to top bar on Files VM

--- SkyDrop/src/SkyDrop.Droid/Views/Main/FilesView.cs
+++ SkyDrop/src/SkyDrop.Droid/Views/Main/FilesView.cs
@@ -22,23 +22,51 @@ namespace SkyDrop.Droid.Views.Main
     {
         protected override int ActivityLayoutId => Resource.Layout.FilesView;
 
+        private IMenu menu;
+
         protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             await ViewModel.InitializeTask.Task;
+            ViewModel.PropertyChanged += HandlePropertyChanged;
 
             Log.Trace("MainView OnCreate()");
 
             ViewModel.SelectFileAsyncFunc = () => AndroidUtil.SelectFile(this);
             ViewModel.SelectImageAsyncFunc = () => AndroidUtil.SelectImage(this);
             ViewModel.FileTapCommand = new MvxCommand<SkyFile>(skyFile => AndroidUtil.OpenFileInBrowser(this, skyFile));
+        }
 
-            //this sets all progressbars in the app to white
-            //I don't think we need this any more as progress bar color should be set in styles.xml
-            var progressBar = FindViewById<ProgressBar>(Resource.Id.ProgressBar);
-            if (progressBar != null)
-                progressBar.IndeterminateDrawable.SetColorFilter(Color.White, PorterDuff.Mode.SrcIn);
+        private void HandlePropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(ViewModel.StagedFiles))
+            {
+                var uploadItem = menu.FindItem(Resource.Id.menu_files_upload);
+                if (ViewModel.StagedFiles.Count > 0)
+                    uploadItem.SetIcon(GetDrawable(Resource.Drawable.ic_upload));
+                else
+                    uploadItem.SetIcon(GetDrawable(Resource.Drawable.ic_upload_grey));
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.FilesMenu, menu);
+            this.menu = menu;
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_files_upload:
+                    ViewModel.UploadCommand?.Execute();
+                    break;
+            }
+
+            return true;
         }
 
         protected override async void OnActivityResult(int requestCode, Android.App.Result resultCode, Intent data)
